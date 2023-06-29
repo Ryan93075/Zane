7c476f3256fb922c1739e97882f8ce28311edfa2
Ryan <129418747+Ryan93075@users.noreply.github.com>
2023-06-29 18:02:06 +0530

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,4 +1,83 @@
-// Chatbot logic
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Chatbot Web Page</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 20px;
+    }
+
+    .container {
+      max-width: 600px;
+      margin: 0 auto;
+      background-color: #fff;
+      border-radius: 8px;
+      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+      padding: 20px;
+    }
+
+    h1 {
+      color: #333;
+      text-align: center;
+    }
+
+    #chat-log {
+      height: 300px;
+      overflow-y: scroll;
+      padding: 10px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      margin-bottom: 10px;
+    }
+
+    .message {
+      padding: 8px;
+      margin-bottom: 10px;
+      border-radius: 4px;
+    }
+
+    .user {
+      background-color: #d9edf7;
+    }
+
+    .bot {
+      background-color: #e2eef0;
+    }
+
+    input[type="text"] {
+      width: 80%;
+      padding: 8px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+    }
+
+    #send-btn {
+      padding: 8px 16px;
+      border: none;
+      background-color: #4caf50;
+      color: #fff;
+      cursor: pointer;
+      border-radius: 4px;
+    }
+
+    #send-btn:hover {
+      background-color: #45a049;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Chatbot Web Page</h1>
+    <div id="chat-log"></div>
+    <input type="text" id="user-input" placeholder="Type your message..." />
+    <button id="send-btn">Send</button>
+  </div>
+
+  <script>
+    // Chatbot logic
 function chatbotMessage(userMessage) {
   // Your chatbot logic goes here
   // You can process the userMessage and generate a response
@@ -44,3 +123,7 @@ function displayMessage(message, sender) {
   messageElement.innerText = message;
   chatLog.appendChild(messageElement);
 }
+
+  </script>
+</body>
+</html>
